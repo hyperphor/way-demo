c46ff3a10b65ce580f64eb0e8e7322336d10b676
Mike Travers <mt@alum.mit.edu>
2026-02-27 15:10:50 -0800

    don't use lein-shadow plugin

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject hyperphor/way-demo "0.2.1" 
   :description "Way"
   :url "https://shrouded-escarpment-03060-744eda4cc53f.herokuapp.com/"
-  :plugins [[lein-shadow "0.4.1"]]
   :dependencies [[org.clojure/clojure "1.12.4"]
                  [com.hyperphor/way "0.2.1"]]
   :main ^:skip-aot hyperphor.way.demo.core
@@ -18,18 +17,4 @@
                                   [day8.re-frame/tracing "0.6.2"]     
                                   [day8.re-frame/re-frame-10x "1.11.0"]]}
              }
-
-  :shadow-cljs {:lein true
-                :builds
-                {:app {:target :browser
-                       :compiler-options {:infer-externs true}
-                       :output-dir "resources/public/cljs-out"
-                       :asset-path "/cljs-out"         ;webserver path
-                       :modules {:dev-main {:entries [hyperphor.way.demo.app]}}
-                       :devtools {:preloads [day8.re-frame-10x.preload.react-18]}
-                       :dev {:compiler-options
-                             {:closure-defines
-                              {re-frame.trace.trace-enabled?        true
-                               day8.re-frame-10x.show-panel         false ;does not work, afaict
-                               day8.re-frame.tracing.trace-enabled? true}}}}}}
   )
